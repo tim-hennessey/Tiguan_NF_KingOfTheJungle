a691b537c041f78869186e1fae91b8a0804e50cc
tim.hennessey <Tim.Hennessey@deutschinc.com>
2017-08-18 17:40:32 -0700

    Creative update

--- app/728x90/scripts/Animation.js
+++ app/728x90/scripts/Animation.js
@@ -38,9 +38,9 @@ app.Animation = (function () {
     // Starts the animation
     function start() {
         tl.to(curtain, .5, {opacity: 0})
-            .to(tig1, 3, {scale: .73, x:"-=32", y:"+=95", ease: Sine.easeInOut}, "+=2")
-            .to(txt1, .5, {opacity: 0}, "-=2")
-            .to(vignette, .5, {opacity: 0}, "-=2")
+            .to(tig1, 3, {scale: .73, x:"-=32", y:"+=95", ease: Sine.easeInOut}, "+=.5")
+            .to(txt1, 1, {opacity: 1}, "-=2")
+            .to(vignette, 1, {opacity: 1}, "-=2")
             .to(hero, 1, {opacity: 1}, "+=3")
             .to(cta_txt, .5, {opacity: 1})
             .to(cta_arrow, .5, {opacity: 1}, "-=.5")
